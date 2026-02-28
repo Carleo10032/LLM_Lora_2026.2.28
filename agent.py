1b36e056c40eeaae3ed9ed32f99f886daf756965
Carleo10032 <3150766822@qq.com>
2026-02-28 12:25:27 +0800

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,44 @@
+from openai import OpenAI
+
+client = OpenAI(
+    base_url='https://ms-ens-f7b6721b-eaf8.api-inference.modelscope.cn/v1',
+    api_key='ms-5c3fba79-bf25-43e1-9445-ac520e1aa3c1',
+)
+
+messages = [
+    {
+        'role': 'system',
+        'content': 'You are a helpful assistant.'
+    }
+]
+
+while True:
+    user_input = input('You: ')
+    if user_input.lower() in ['exit', 'quit']:
+        break
+    
+    messages.append({
+        'role': 'user',
+        'content': user_input
+    })
+    
+    response = client.chat.completions.create(
+        model='lqw1203/private_model_06B',
+        messages=messages,
+        stream=True
+    )
+    
+    print('Assistant: ', end='', flush=True)
+    assistant_response = ''
+    for chunk in response:
+        if chunk.choices:
+            content = chunk.choices[0].delta.content
+            if content:
+                print(content, end='', flush=True)
+                assistant_response += content
+    print()
+    
+    messages.append({
+        'role': 'assistant',
+        'content': assistant_response
+    })
